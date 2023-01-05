f448d4bd95440a5731e4001ae1d1dd862ce1e554
Daniel Black <danielblack@Daniels-MacBook-Air.local>
2023-01-05 10:59:00 +0700

    improve mobile layout

--- src/App.js
+++ src/App.js
@@ -295,7 +295,7 @@ const App = () => {
               {sliceIntoChunks(categories.trivia_categories, 4).map(chunk => 
                 <div className='grid row'>
                   {chunk.map(category => 
-                    <div className="col-3 pb-4">
+                    <div className="col-6 col-md-3 pb-4">
                       <button
                       onClick={() => {setGameStarted(true); setSelectedCategoryId(category.id); setSelectedCategoryName(category.name)}}
                       className='btn w-100 rounded d-flex align-items-center justify-content-center' style={{minHeight: '90px', background: '#efefef'}}>
